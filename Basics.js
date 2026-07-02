82b89148a04d64e8095b236e6f8adf01990e1aa9
Aditya Raj <adityaraj34@icloud.com>
2026-07-02 22:49:18 +0530

    check

---
+++ Basics.js
@@ -0,0 +1,23 @@
+"use strict";
+// let names: string = "John Doe";
+// let age: number = 25;
+// let isStudent: boolean = true;
+// let tags: string[] = ["typescript"];
+// let anythiong: any = "This can be any type but avoid using it as much as possible";
+// let unkownValue: unknown = "This can be any type but you need to check the type before using it";
+console.log("Hello, TypeScript!");
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 15));
+function greet(name, greeting = "Hello") {
+    return `${greeting}, ${name}!`;
+}
+console.log(greet("John")); // "Hello, John!"
+const user = {
+    name: "Aditya",
+    age: 25,
+    address: "123 Main St",
+    gender: "male"
+};
+console.log(user);
---
+++ Basics.ts
@@ -0,0 +1,36 @@
+// let names: string = "John Doe";
+// let age: number = 25;
+// let isStudent: boolean = true;
+// let tags: string[] = ["typescript"];
+// let anythiong: any = "This can be any type but avoid using it as much as possible";
+// let unkownValue: unknown = "This can be any type but you need to check the type before using it";
+
+console.log("Hello, TypeScript!");
+
+
+function add(a:number ,b:number): number {
+    return a + b;
+}
+console.log(add(5,15));
+
+function greet(name:string ,greeting:string = "Hello"):string {
+    return`${greeting}, ${name}!`;
+}
+
+console.log(greet("John")); // "Hello, John!"
+
+
+interface User {
+    name:string,
+    age:number,
+    address?: string // optional property
+    gender: "male" | "female"
+}
+
+const user :User ={
+    name:"Aditya",
+    age: 25,
+    address: "123 Main St",
+    gender:"male"
+}
+console.log(user);
