360c968726e0200eb0a8c572084cb8509e957244
Aditya Raj <adityaraj34@icloud.com>
2026-07-04 22:46:21 +0530

    generisjs

--- Generics.js
+++ Generics.js
@@ -1,5 +1,8 @@
 "use strict";
 //Generics
-function identity(arg) {
-    return arg;
+// hum eak function ko multiple types ke sath use kar sakte hai
+function test(a) {
+    console.log(a);
 }
+test("Hello World");
+test(123);
