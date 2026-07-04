2c561280d515b2dc5203868e2fa8529b443c50aa
Aditya Raj <adityaraj34@icloud.com>
2026-07-04 22:46:04 +0530

    generis t

--- Generics.ts
+++ Generics.ts
@@ -1,4 +1,8 @@
 //Generics
-function identity<T>(arg: T): T {
-    return arg;
+// hum eak function ko multiple types ke sath use kar sakte hai
+function test<T>(a:T):T{
+    console.log(a);
 }
+
+test<string>("Hello World");
+test<number>(123);
