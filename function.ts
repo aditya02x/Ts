ad15c053ea4e9e075a09e1ac12bdcce55b4b4083
Aditya Raj <adityaraj34@icloud.com>
2026-07-04 21:32:52 +0530

    para

--- function.ts
+++ function.ts
@@ -1,3 +1,11 @@
 function abcdef(name: string) {
     console.log(`Hello, ${name}`);
 }
+
+
+//optional parameters
+function info(name:string , age:number ,gender: string="not specified") {
+    console.log(`Name: ${name}, Age: ${age}, Gender: ${gender}`);
+}
+info("aditya",25,"male");
+info("test",30); // Uses the default gender value
