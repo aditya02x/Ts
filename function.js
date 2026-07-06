fba1d36dd40992050cf5a375d7830e6c3109df05
Aditya Raj <adityaraj34@icloud.com>
2026-07-06 23:59:17 +0530

    dssdgadafsf

--- function.js
+++ function.js
@@ -14,4 +14,12 @@ function sum(...arr) {
 }
 sum(1, 2, 3, 4, 5); // [1, 2, 3, 4, 5]
 function abcde(a) {
-    if (typeof a === "string
+    if (typeof a === "string") {
+        console.log(`String: ${a}`);
+    }
+    else if (typeof a === "number") {
+        console.log(`Number: ${a}`);
+    }
+}
+abcde("Hello");
+abcde(42);
