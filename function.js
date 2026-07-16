17c70c601411b74e07ea1acbd4ea1fde987a47ea
Aditya Raj <adityaraj34@icloud.com>
2026-07-16 23:53:42 +0530

    dhek

--- function.js
+++ function.js
@@ -11,6 +11,10 @@ info("test", 30); // Uses the default gender value
 // ...rest parameters
 function sum(...arr) {
     console.log(arr);
+}
+sum(1, 2, 3, 4, 5); // [1, 2, 3, 4, 5]
+function abcde(a) {
+    if (typeof a === "string") {
         console.log(`String: ${a}`);
     }
     else if (typeof a === "number") {
