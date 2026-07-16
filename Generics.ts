102f9503dc8cbfad0671db9c5c6ac7a70bb21b2a
Aditya Raj <adityaraj34@icloud.com>
2026-07-16 23:51:14 +0530

    dfda

--- Generics.ts
+++ Generics.ts
@@ -1,10 +1,7 @@
 //Generics
 // hum eak function ko multiple types ke sath use kar sakte hai
 function test<T>(a:T):T{
-    console.log(a);
-}
-
-test<string>("Hello World");
+    console.log(a)
 test<number>(123);
 
 
