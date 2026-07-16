f82842ff4bcd978d04f7ce374064e49092a8b72d
Aditya Raj <adityaraj34@icloud.com>
2026-07-16 23:52:49 +0530

    djghd

--- function.js
+++ function.js
@@ -3,6 +3,10 @@ function abcdef(name) {
     console.log(`Hello, ${name}`);
 }
 //optional parameters
+function info(name, age, gender = "not specified") {
+    console.log(`Name: ${name}, Age: ${age}, Gender: ${gender}`);
+}
+info("aditya", 25, "male");
 info("test", 30); // Uses the default gender value
 // ...rest parameters
 function sum(...arr) {
