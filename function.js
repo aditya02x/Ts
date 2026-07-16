6d5d5c1b68bb7d1a5e5cdae740e09b9bb7211328
Aditya Raj <adityaraj34@icloud.com>
2026-07-16 23:51:51 +0530

    dfdadddd

--- function.js
+++ function.js
@@ -7,12 +7,6 @@ function info(name, age, gender = "not specified") {
     console.log(`Name: ${name}, Age: ${age}, Gender: ${gender}`);
 }
 info("aditya", 25, "male");
-info("test", 30); // Uses the default gender value
-// ...rest parameters
-function sum(...arr) {
-    console.log(arr);
-}
-sum(1, 2, 3, 4, 5); // [1, 2, 3, 4, 5]
 function abcde(a) {
     if (typeof a === "string") {
         console.log(`String: ${a}`);
